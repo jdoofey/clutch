85f2525a21963b7ac79dd2009223b5378aa64fd2
jake matillano <jakematillano.z@gmail.com>
2023-07-06 12:50:59 -0700

    functioning dropdown menu with navigation bar adjustments

--- my-projects/clutch/react-app/src/components/Navigation/index.js
+++ my-projects/clutch/react-app/src/components/Navigation/index.js
@@ -35,6 +35,33 @@ function Navigation() {
 		<>
 			<div className='nav-main'>
 				<div className="nav-title">Clutch</div>
+
+
+				<ul class="nav-links">
+
+
+
+					<div class="menu">
+
+						<li><a href="/">Something</a></li>
+						<li><a href="/">Another one</a></li>
+
+						<li class="dropdown-list-ele">
+							<a href="/">Dropdown</a>
+							<ul class="dropdown">
+								<li><a href="/">Dropdown 1 </a></li>
+								<li><a href="/">Dropdown 2</a></li>
+								<li><a href="/">Dropdown 2</a></li>
+								<li><a href="/">Dropdown 3</a></li>
+								<li><a href="/">Dropdown 4</a></li>
+							</ul>
+
+						</li>
+
+						<li><a href="/">+1</a></li>
+						<li><a href="/">Contact</a></li>
+					</div>
+				</ul>
 				{sessionLinks}
 			</div>
 		</>
